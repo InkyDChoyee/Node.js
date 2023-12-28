bb7ed302b8d29023c966b5d5fb6810ee6e2fa6fc
InkyDChoyee <choyee0421@naver.com>
2023-12-28 20:47:15 +0900

    feat: module_exports

--- calc.js
+++ calc.js
@@ -1,3 +1,10 @@
-const add = (a, b) => a + b;
+// 계산 기능을 하는 파일 = 계산 모듈
 
-console.log(add(1, 2));
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+module.exports = {
+    moduleName: "calc module",
+    add: add,
+    sub: sub,
+};  // 계산 모듈 객체 내보내기
